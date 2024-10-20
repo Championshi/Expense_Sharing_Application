79f2520244904ab35e12f7109137d93743e43f23
Abhinav Kumar <135168416+Championshi@users.noreply.github.com>
2024-10-20 19:10:46 +0530

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createUser, getAllUsers } = require('../controllers/userController');
 
-// POST /users: Create a new user
 router.post('/', createUser);
-
-// GET /users: Retrieve all users
 router.get('/', getAllUsers);
 
 module.exports = router;
