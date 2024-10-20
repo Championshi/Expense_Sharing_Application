78d113c18d3f8d013c56d09628cf9003b2105760
Abhinav Kumar <135168416+Championshi@users.noreply.github.com>
2024-10-20 19:10:19 +0530

    Update expense.js

--- Routes/expense.js
+++ Routes/expense.js
@@ -2,11 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { addExpense, getAllExpenses } = require('../controllers/expenseController');
-
-// POST /expenses: Add a new expense
 router.post('/', addExpense);
-
-// GET /expenses: Retrieve all expenses
 router.get('/', getAllExpenses);
 
 module.exports = router;
